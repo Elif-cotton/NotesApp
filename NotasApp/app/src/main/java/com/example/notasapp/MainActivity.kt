f3970b803e28a53d161fcbb2dbdee1db76b230fa
“elif-cotton” <elif.watercolors@gmail.com>
2024-08-10 18:53:06 -0400

    Terminado NotesA

--- NotasApp/app/src/main/java/com/example/notasapp/MainActivity.kt
+++ NotasApp/app/src/main/java/com/example/notasapp/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var db : NotesDataBaseHelper
+    private lateinit var notesAdapter: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +21,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NotesDataBaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter= notesAdapter
+
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refeshData(db.getAllNotes())
     }
 }
